baa95d402bc2066cdea66fe73121416fe1ad424e
landonr7 <landon.reynolds777@gmail.com>
2024-08-22 12:04:12 -0500

    added comments

--- NoteCreator.bat
+++ NoteCreator.bat
@@ -9,13 +9,8 @@ echo date: %today%
 :: Get and set hour to an integer
 set "hour=%time:~0,2%
 if "%hour:~0,1%"==" " set "hour=%hour:~1,1%"
-::echo hour: %hour%
 
-:: Makes individual number codes for each class
-set /A class= 24 - %hour%
-::echo class: %class%
-
-
+:: Time of day
 set "min_sec=%time:~0,5%"
 set num_time=%min_sec::=%
 echo %num_time%
@@ -24,40 +19,43 @@ echo %num_time%
 set "dotw=%date:~0,3%"
 echo %dotw%
 
+:: Depending on day, access certain portion of code
 if "%dotw%" equ "Mon" goto MWF
 if "%dotw%" equ "Tue" goto TuTh
 if "%dotw%" equ "Wed" goto MWF
 if "%dotw%" equ "Thu" goto TuTh
 if "%dotw%" equ "Fri" goto MWF
 
+:: Monday, Wednesday, Friday
 :MWF
 echo in MWF
 
 if !num_time! geq 835 if !num_time! leq 925 (
-
+    :: INEG
     echo ineg
     set "notes=INEGNotes"
     goto end
 
 ) else if !num_time! geq 940 if !num_time! leq 1030 (
-
+    :: Information Retrieval
     echo ir
     set "notes=IRNotes"
     goto end
 
 ) else if !num_time! geq 1400 if !num_time! leq 1450 (
-
+    :: Embedded Systems
     echo es
     set "notes=ESNotes"
     goto end
 
 ) else if !num_time! geq 1505 if !num_time! leq 1555 (
-
+    :: Capstone
     echo cs
     set "notes=CSNotes"
     goto end
 
 ) else (
+    :: Miscelaneous
     goto else
 )
 
@@ -65,7 +63,7 @@ if !num_time! geq 835 if !num_time! leq 925 (
 echo in TuTh
 
 if !num_time! geq 1100 if !num_time! leq 1150 (
-
+    :: Datamining
     echo dm
     set "notes=DMNotes"
     goto end
@@ -82,11 +80,11 @@ if !num_time! geq 1100 if !num_time! leq 1150 (
 
 :end
 
+:: Where the text file will be
 set "file_path=C:\Users\valor\Desktop\FALL2024-25\!notes!\!notes!%today%.txt"
-set "insert_text=!notes! %today%:"
 
-echo %file_path%
-echo %insert_text%
+:: Text inserted into the file
+set "insert_text=!notes! %today%:"
 
 :: Define the text file and the text to insert
 if not exist "%file_path%" (
@@ -103,5 +101,5 @@ if not exist "%file_path%" (
 
 )
 
-pause
+exit
 
