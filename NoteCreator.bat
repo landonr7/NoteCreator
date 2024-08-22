7e6c0108a2ff8d7b59e18c1554594400c82b47e6
landonr7 <landon.reynolds777@gmail.com>
2024-08-22 09:34:27 -0500

    Created NoteCreator.bat

---
+++ NoteCreator.bat
@@ -0,0 +1,107 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+:: Gets today's date without DOTW
+set today=%date:~4%
+set today=%today:/=-%
+echo date: %today%
+
+:: Get and set hour to an integer
+set "hour=%time:~0,2%
+if "%hour:~0,1%"==" " set "hour=%hour:~1,1%"
+::echo hour: %hour%
+
+:: Makes individual number codes for each class
+set /A class= 24 - %hour%
+::echo class: %class%
+
+
+set "min_sec=%time:~0,5%"
+set num_time=%min_sec::=%
+echo %num_time%
+
+:: Day of the week
+set "dotw=%date:~0,3%"
+echo %dotw%
+
+if "%dotw%" equ "Mon" goto MWF
+if "%dotw%" equ "Tue" goto TuTh
+if "%dotw%" equ "Wed" goto MWF
+if "%dotw%" equ "Thu" goto TuTh
+if "%dotw%" equ "Fri" goto MWF
+
+:MWF
+echo in MWF
+
+if !num_time! geq 835 if !num_time! leq 925 (
+
+    echo ineg
+    set "notes=INEGNotes"
+    goto end
+
+) else if !num_time! geq 940 if !num_time! leq 1030 (
+
+    echo ir
+    set "notes=IRNotes"
+    goto end
+
+) else if !num_time! geq 1400 if !num_time! leq 1450 (
+
+    echo es
+    set "notes=ESNotes"
+    goto end
+
+) else if !num_time! geq 1505 if !num_time! leq 1555 (
+
+    echo cs
+    set "notes=CSNotes"
+    goto end
+
+) else (
+    goto else
+)
+
+:TuTh
+echo in TuTh
+
+if !num_time! geq 1100 if !num_time! leq 1150 (
+
+    echo dm
+    set "notes=DMNotes"
+    goto end
+
+) else (
+    goto else
+)
+
+:else
+
+    echo misc
+    set "notes=MISCNotes"
+    goto end
+
+:end
+
+set "file_path=C:\Users\valor\Desktop\FALL2024-25\!notes!\!notes!%today%.txt"
+set "insert_text=!notes! %today%:"
+
+echo %file_path%
+echo %insert_text%
+
+:: Define the text file and the text to insert
+if not exist "%file_path%" (
+
+    echo File does not exist. Creating a new file...
+    echo %insert_text% >> "%file_path%"
+
+    :: Opens note in vscode
+    code %file_path%
+
+) else (
+
+    code %file_path%
+
+)
+
+pause
+
